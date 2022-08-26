42c648e20a06f70b486ce6fdefc07e536c69e702
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-08-26 15:16:15 -0500

    initial commit

---
+++ csharp/fizzbuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int n=1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                    Console.WriteLine("fizzbuzz");
+                else if (n % 3 == 0)
+                    Console.WriteLine("fizz");
+                else if (n % 5 == 0)
+                    Console.WriteLine("fizz");
+                else
+                    Console.WriteLine(n);
+            }
+        }
+    }
+}
